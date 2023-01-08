45e32cc6ea6ac425c9a11a85037357b63f67c33b
frisk16 <test-n18@hotmail.co.jp>
2023-01-08 21:06:31 +0900

    20230108-edit8

--- app/Database.php
+++ app/Database.php
@@ -6,12 +6,18 @@ class Database
   {
     try {
       $pdo = new PDO(
+        // 'mysql:
+        // dbname=heroku_c7bfd9044b0a20e;
+        // host=us-cdbr-east-06.cleardb.net;
+        // charset=utf8mb4',
+        // 'b7144df2232837',
+        // '8368b769',
         'mysql:
-        dbname=heroku_c7bfd9044b0a20e;
-        host=us-cdbr-east-06.cleardb.net;
+        dbname=master_db;
+        host=localhost;
         charset=utf8mb4',
-        'b7144df2232837',
-        '8368b769',
+        'frisk',
+        'Nto1160!',
         [
           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
